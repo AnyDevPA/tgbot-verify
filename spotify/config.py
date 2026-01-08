e5af90d299732a069ad5e679a1c0f4f1aafd42c2
BRIAN JOSE AGUILAR <baguilar78@alumno.uned.es>
2026-01-08 02:18:28 -0700

    Update config.py

--- spotify/config.py
+++ spotify/config.py
@@ -1,19 +1,21 @@
 # SheerID 验证配置文件
 
 # SheerID API 配置
-PROGRAM_ID = '695f63e8f44326081137a4e8' 
+PROGRAM_ID = '695f63e8f44326081137a4e8'
 SHEERID_BASE_URL = 'https://services.sheerid.com'
 MY_SHEERID_URL = 'https://my.sheerid.com'
 
 # 文件大小限制
 MAX_FILE_SIZE = 1 * 1024 * 1024  # 1MB
 
-# 学校配置
+# ID REAL QUE ENCONTRASTE EN LA CAPTURA
+UNAM_ID = 415489  # <--- ESTE ES EL BUENO
+
 SCHOOLS = {
-    # --- AGREGADO: UNAM (MEXICO) ---
-    '5032': {  # <--- ESTE ES EL ID, CONFIRMA SI ES 5032 EN TU NAVEGADOR
-        'id': 5032,
-        'idExtended': '5032',
+    str(UNAM_ID): {
+        'id': UNAM_ID,
+        'idExtended': str(UNAM_ID),
+        # El nombre exacto que sale en tu captura
         'name': 'Universidad Nacional Autónoma De México (UNAM)',
         'city': 'Coyoacán',
         'state': 'CDMX',
@@ -22,21 +24,8 @@ SCHOOLS = {
         'domain': 'UNAM.MX',
         'latitude': 19.332,
         'longitude': -99.186
-    },
-    # -------------------------------
-    '2565': {
-        'id': 2565,
-        'idExtended': '2565',
-        'name': 'Pennsylvania State University-Main Campus',
-        'city': 'University Park',
-        'state': 'PA',
-        'country': 'US',
-        'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.798214,
-        'longitude': -77.85991
     }
 }
 
-# 默认学校 (CAMBIADO A UNAM)
-DEFAULT_SCHOOL_ID = '5032'
+# Configurar este ID como el default
+DEFAULT_SCHOOL_ID = str(UNAM_ID)
