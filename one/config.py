4bd6de4fc86318226b91656c866209b7598d88af
BRIAN JOSE AGUILAR <baguilar78@alumno.uned.es>
2026-01-08 23:25:12 -0700

    Update config.py

--- one/config.py
+++ one/config.py
@@ -1,23 +1,23 @@
-# one/config.py - MODO WESTERN TECH (Fake Edu Email)
+# one/config.py - MODO PRO (School Email)
 
 PROGRAM_ID = '67c8c14f5f17a83b745e3f82'
 SHEERID_BASE_URL = 'https://services.sheerid.com'
 
-# Western Technical College (El Paso, TX)
-# ID: 4078874
+# Western Governors University (Salt Lake City, UT)
+# ID: 3919 (Confirmado)
 SCHOOLS = {
-    '4078874': {
-        'id': 4078874,
-        'name': 'Western Technical College (El Paso, TX)',
-        'city': 'El Paso',
-        'state': 'TX',
+    '3919': {
+        'id': 3919,
+        'name': 'Western Governors University (Salt Lake City, UT)',
+        'city': 'Salt Lake City',
+        'state': 'UT',
         'country': 'US',
         'type': 'UNIVERSITY',
-        'domain': 'westerntech.edu' # <--- DOMINIO CLAVE
+        'domain': 'wgu.edu'  # <--- EL DOMINIO MÁGICO
     }
 }
 
-DEFAULT_SCHOOL_ID = '4078874'
+DEFAULT_SCHOOL_ID = '3919'
 
 METADATA = {
     "marketConsentValue": False,
