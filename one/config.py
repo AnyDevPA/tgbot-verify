5fb3affe0f3e1b32426fcf04bf524dbd9026cdd6
BRIAN JOSE AGUILAR <baguilar78@alumno.uned.es>
2026-01-08 23:06:38 -0700

    Update config.py

--- one/config.py
+++ one/config.py
@@ -1,22 +1,22 @@
-# one/config.py - MODO WGU (ID CONFIRMADO 3919)
+# one/config.py - MODO WESTERN TECH (Low Security)
 
 PROGRAM_ID = '67c8c14f5f17a83b745e3f82'
 SHEERID_BASE_URL = 'https://services.sheerid.com'
 
-# Western Governors University (Salt Lake City, UT)
-# ID CONFIRMADO POR F12: 3919
+# Western Technical College (El Paso, TX)
+# ID sacado de tu captura: 4078874
 SCHOOLS = {
-    '3919': {
-        'id': 3919,
-        'name': 'Western Governors University (Salt Lake City, UT)',
-        'city': 'Salt Lake City',
-        'state': 'UT',
+    '4078874': {
+        'id': 4078874,
+        'name': 'Western Technical College (El Paso, TX)',
+        'city': 'El Paso',
+        'state': 'TX',
         'country': 'US',
-        'type': 'UNIVERSITY'
+        'type': 'UNIVERSITY' # En SheerID suelen catalogarlas así
     }
 }
 
-DEFAULT_SCHOOL_ID = '3919'
+DEFAULT_SCHOOL_ID = '4078874'
 
 METADATA = {
     "marketConsentValue": False,
